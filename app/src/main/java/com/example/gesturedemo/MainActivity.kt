e822d649e45fa3bfe630495dc972f3d0d8001d5b
arina <arinademikhova@gmail.com>
2025-12-07 10:00:00 +0300

    start project

---
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.gesturedemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.gesturedemo.ui.theme.GestureDemoTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GestureDemoTheme {
+                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
+                    MainScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+    ClickDemo(modifier)
+}
+@Composable
+fun ClickDemo(modifier: Modifier = Modifier) {
+    var colorState by remember { mutableStateOf(true) }
+    var bgColor by remember { mutableStateOf(Color.Blue) }
+
+    val clickHandler = {
+
+        colorState = !colorState
+        bgColor = if (colorState) {
+            Color.Blue
+        } else {
+            Color.DarkGray
+        }
+    }
+
+    Box(
+        modifier
+            .clickable() { clickHandler() }
+            .background(bgColor)
+            .size(100.dp)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GestureDemoTheme {
+        MainScreen()
+    }
+}
